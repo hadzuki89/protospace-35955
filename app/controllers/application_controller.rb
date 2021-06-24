de30f493b83d42a54dce98c790ed5e8eabcad248
hadzuki89 <k_hadzuki89@icloud.com>
2021-06-24 16:44:07 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+  end
+end
