c7425a33cb2619fb7758f62f877c0db4316c5ac1
hadzuki89 <k_hadzuki89@icloud.com>
2021-06-25 13:33:06 +0900

    fourth_commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #@name = prototype.user.name
-    #@profile = current_user.profile
-    #@occupation = current_user.occupation
-    #@position = current_user.position
-    #@prototypes = current_user.prototypes
   end
 
 end
